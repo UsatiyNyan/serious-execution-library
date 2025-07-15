6e580f46e7456efe5a3308ff62dc4e6ba7eebfa6
UsatiyNyan <us4tiyny4n@gmail.com>
2025-07-15 12:20:00 +0200

    thread: detail: multiword_dcss: improve testing

--- include/sl/exec/thread/detail/multiword_dcss.hpp
+++ include/sl/exec/thread/detail/multiword_dcss.hpp
@@ -16,6 +16,13 @@ namespace sl::exec::detail {
 // (std::uintptr_t) -> uintptr_t
 using dcss_a1_load_type = std::uintptr_t (*)(std::uintptr_t);
 
+template <typename T>
+std::uintptr_t dcss_a1_load_default(std::uintptr_t a1_erased) {
+    auto* a1 = std::bit_cast<detail::atomic<T>*>(a1_erased);
+    const auto result = a1->load(std::memory_order::relaxed);
+    return std::bit_cast<std::uintptr_t>(result);
+}
+
 struct dcss_descriptor {
     // concept requirements
     static constexpr std::size_t max_threads = mw::default_max_threads;
@@ -40,6 +47,14 @@ public:
     immutables_type immutables{};
 };
 
+mw::pointer_type dcss_create_new(
+    std::uintptr_t a1,
+    dcss_a1_load_type a1_load,
+    std::uintptr_t e1,
+    detail::atomic<std::uintptr_t>* a2,
+    std::uintptr_t e2,
+    std::uintptr_t n2
+);
 std::uintptr_t dcss(
     std::uintptr_t a1,
     dcss_a1_load_type a1_load,
@@ -61,7 +76,7 @@ template <typename T>
 T dcss(detail::atomic<T>& a1, T e1, detail::atomic<T>& a2, T e2, T n2) {
     std::uintptr_t result = detail::dcss(
         std::bit_cast<std::uintptr_t>(&a1),
-        [](std::uintptr_t a1) { return std::bit_cast<detail::atomic<T>*>(a1)->load(std::memory_order::relaxed); },
+        dcss_a1_load_default<T>,
         std::bit_cast<std::uintptr_t>(e1),
         std::bit_cast<detail::atomic<std::uintptr_t>*>(&a2),
         std::bit_cast<std::uintptr_t>(e2),
