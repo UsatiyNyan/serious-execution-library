40143619928e2f644bcd9e759ddda1b5f507cf8d
UsatiyNyan <us4tiyny4n@gmail.com>
2026-03-22 23:49:44 +0100

    model change: executor

--- include/sl/exec/model/executor.hpp
+++ include/sl/exec/model/executor.hpp
@@ -10,8 +10,18 @@ namespace sl::exec {
 
 struct executor {
     virtual ~executor() noexcept = default;
-    virtual void schedule(task_node* task_node) noexcept = 0;
+    virtual void schedule(task_node& a_task_node) noexcept = 0;
     virtual void stop() noexcept = 0;
 };
+
+inline executor& inline_executor() {
+    struct impl final : executor {
+        constexpr void schedule(task_node& a_task_node) noexcept override { a_task_node.execute(); }
+        constexpr void stop() noexcept override {}
+    };
+
+    static impl an_executor;
+    return an_executor;
+}
 
 } // namespace sl::exec
