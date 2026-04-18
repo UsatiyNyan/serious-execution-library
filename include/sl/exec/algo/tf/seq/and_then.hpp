043d353be79fc72d7be16b0102deb2982cf52d92
UsatiyNyan <us4tiyny4n@gmail.com>
2026-04-18 19:38:01 +0200

    static and noexcept: tf/seq

--- include/sl/exec/algo/tf/seq/and_then.hpp
+++ include/sl/exec/algo/tf/seq/and_then.hpp
@@ -4,8 +4,6 @@
 
 #pragma once
 
-#include "sl/exec/algo/emit/subscribe.hpp"
-
 #include "sl/exec/model/concept.hpp"
 #include "sl/exec/model/executor.hpp"
 
@@ -15,8 +13,8 @@
 namespace sl::exec {
 namespace detail {
 
-template <typename InputValueT, typename ValueT, typename ErrorT, typename F>
-struct [[nodiscard]] and_then_slot final : slot<InputValueT, ErrorT> {
+template <typename InputValueT, typename ValueT, typename ErrorT, typename F, typename SlotT>
+struct [[nodiscard]] and_then_slot final {
     struct and_then_task final : task_node {
         explicit and_then_task(and_then_slot& self) : self_{ self } {}
 
@@ -26,34 +24,58 @@ struct [[nodiscard]] and_then_slot final : slot<InputValueT, ErrorT> {
             }
             auto result = self_.functor_(std::move(self_.maybe_value_).value());
             if (result.has_value()) {
-                self_.slot_.set_value(std::move(result).value());
+                std::move(self_.slot_).set_value(std::move(result).value());
             } else {
-                self_.slot_.set_error(std::move(result).error());
+                std::move(self_.slot_).set_error(std::move(result).error());
             }
         }
-        void cancel() noexcept override { self_.set_null(); };
+        void cancel() noexcept override { std::move(self_).set_null(); }
 
     private:
         and_then_slot& self_;
     };
 
-    and_then_slot(F&& functor, slot<ValueT, ErrorT>& slot, executor& executor)
-        : functor_{ std::move(functor) }, slot_{ slot }, executor_{ executor } {}
+    F functor_;
+    SlotT slot_;
+    executor& executor_;
+    meta::maybe<InputValueT> maybe_value_{};
+    meta::maybe<and_then_task> maybe_task_{};
 
-    void set_value(InputValueT&& value) & override {
+    void set_value(InputValueT&& value) && noexcept {
         maybe_value_.emplace(std::move(value));
         auto& task = maybe_task_.emplace(*this);
         executor_.schedule(task);
     }
-    void set_error(ErrorT&& error) & override { slot_.set_error(std::move(error)); }
-    void set_null() & override { slot_.set_null(); }
+    void set_error(ErrorT&& error) && noexcept { std::move(slot_).set_error(std::move(error)); }
+    void set_null() && noexcept { std::move(slot_).set_null(); }
+};
 
-private:
-    F functor_;
-    meta::maybe<InputValueT> maybe_value_{};
-    meta::maybe<and_then_task> maybe_task_{};
-    slot<ValueT, ErrorT>& slot_;
-    executor& executor_;
+template <SomeSignal SignalT, typename F, typename SlotCtorT, typename ResultT = std::invoke_result_t<F, typename SignalT::value_type>>
+    requires std::same_as<typename SignalT::error_type, typename ResultT::error_type>
+struct [[nodiscard]] and_then_connection final {
+    using input_value_type = typename SignalT::value_type;
+    using value_type = typename ResultT::value_type;
+    using error_type = typename ResultT::error_type;
+    using SlotT = SlotFrom<SlotCtorT>;
+    using and_then_slot_type = and_then_slot<input_value_type, value_type, error_type, F, SlotT>;
+
+    struct and_then_slot_ctor {
+        F functor;
+        SlotCtorT slot_ctor;
+        executor& ex;
+
+        constexpr and_then_slot_type operator()() && noexcept {
+            return and_then_slot_type{
+                .functor_ = std::move(functor),
+                .slot_ = std::move(slot_ctor)(),
+                .executor_ = ex,
+            };
+        }
+    };
+
+    ConnectionFor<SignalT, and_then_slot_ctor> connection;
+
+    constexpr CancelHandle auto emit() && noexcept { return std::move(connection).emit(); }
 };
 
 template <SomeSignal SignalT, typename F, typename ResultT = std::invoke_result_t<F, typename SignalT::value_type>>
@@ -61,21 +83,24 @@ template <SomeSignal SignalT, typename F, typename ResultT = std::invoke_result_
 struct [[nodiscard]] and_then_signal final {
     using value_type = typename ResultT::value_type;
     using error_type = typename ResultT::error_type;
-    using slot_type = and_then_slot<typename SignalT::value_type, value_type, error_type, F>;
 
 public:
     constexpr and_then_signal(SignalT&& signal, F&& functor)
         : signal_{ std::move(signal) }, functor_{ std::move(functor) } {}
 
-    subscribe_connection<SignalT, slot_type> subscribe(slot<value_type, error_type>& slot) && {
-        executor& executor = signal_.get_executor();
-        return subscribe_connection<SignalT, slot_type>{
-            std::move(signal_),
-            [&] { return slot_type{ std::move(functor_), slot, executor }; },
+    template <SlotCtor<value_type, error_type> SlotCtorT>
+    constexpr Connection auto subscribe(SlotCtorT&& slot_ctor) && noexcept {
+        using ConnectionT = and_then_connection<SignalT, F, SlotCtorT>;
+        using SlotCtorForSignal = typename ConnectionT::and_then_slot_ctor;
+        executor& ex = signal_.get_executor();
+        return ConnectionT{
+            .connection = std::move(signal_).subscribe(
+                SlotCtorForSignal{ std::move(functor_), std::move(slot_ctor), ex }
+            ),
         };
     }
 
-    executor& get_executor() { return signal_.get_executor(); }
+    constexpr executor& get_executor() noexcept { return signal_.get_executor(); }
 
 private:
     SignalT signal_;
@@ -87,7 +112,7 @@ struct [[nodiscard]] and_then final {
     constexpr explicit and_then(F&& functor) : functor_{ std::move(functor) } {}
 
     template <SomeSignal SignalT>
-    constexpr SomeSignal auto operator()(SignalT&& signal) && {
+    constexpr SomeSignal auto operator()(SignalT&& signal) && noexcept {
         return and_then_signal<SignalT, F>{ std::move(signal), std::move(functor_) };
     }
 
@@ -98,7 +123,7 @@ private:
 } // namespace detail
 
 template <typename F>
-constexpr auto and_then(F functor) {
+constexpr auto and_then(F functor) noexcept {
     return detail::and_then<F>{ std::move(functor) };
 }
 
