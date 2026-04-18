6750f6d68651a76c8ea104fe324e30373a172746
UsatiyNyan <us4tiyny4n@gmail.com>
2026-04-18 20:12:36 +0200

    static and noexcept: coro

--- include/sl/exec/coro/as_signal.hpp
+++ include/sl/exec/coro/as_signal.hpp
@@ -14,37 +14,37 @@
 
 namespace sl::exec::detail {
 
-template <typename ValueT, typename ErrorT>
-struct [[nodiscard]] async_connection final : connection {
+template <typename ValueT, typename ErrorT, typename SlotT>
+struct [[nodiscard]] async_connection final {
 public:
-    constexpr async_connection(async<ValueT> async, slot<ValueT, ErrorT>& slot, executor& executor)
-        : async_{ std::move(async) }, slot_{ slot }, executor_{ executor } {}
+    constexpr async_connection(async<ValueT> an_async, SlotT slot, executor& executor)
+        : async_{ std::move(an_async) }, slot_{ std::move(slot) }, executor_{ executor } {}
 
-    cancel_handle& emit() && noexcept override {
-        coro_schedule(executor_, make_connection_coro(std::move(async_), slot_));
-        return dummy_cancel_handle();
+    CancelHandle auto emit() && noexcept {
+        coro_schedule(executor_, make_connection_coro(std::move(async_), std::move(slot_)));
+        return dummy_cancel_handle{};
     }
 
 private:
-    static async<void> make_connection_coro(async<ValueT> async, slot<ValueT, ErrorT>& slot) {
+    static async<void> make_connection_coro(async<ValueT> an_async, SlotT slot) {
         // coroutine handle can be destroyed at any point, when executor destroys it's tasks
         bool is_fulfilled = false;
         const meta::defer set_null_if_not_fulfilled{ [&] {
             if (!is_fulfilled) {
-                slot.set_null();
+                std::move(slot).set_null();
             }
         } };
         try {
-            slot.set_value(co_await std::move(async));
+            std::move(slot).set_value(co_await std::move(an_async));
         } catch (...) {
-            slot.set_error(std::current_exception());
+            std::move(slot).set_error(std::current_exception());
         }
         is_fulfilled = true;
     }
 
 private:
     async<ValueT> async_;
-    slot<ValueT, ErrorT>& slot_;
+    SlotT slot_;
     executor& executor_;
 };
 
@@ -54,17 +54,19 @@ struct [[nodiscard]] async_signal final {
     using error_type = std::exception_ptr;
 
 public:
-    constexpr explicit async_signal(async<T> async) : async_{ std::move(async) } {}
+    constexpr explicit async_signal(async<T> an_async) : async_{ std::move(an_async) } {}
 
-    async_connection<value_type, error_type> subscribe(slot<value_type, error_type>& slot) && {
-        return async_connection<value_type, error_type>{
-            /* .result = */ std::move(async_),
-            /* .slot = */ slot,
-            /* .executor = */ get_executor(),
+    template <SlotCtor<value_type, error_type> SlotCtorT>
+    constexpr Connection auto subscribe(SlotCtorT&& slot_ctor) && noexcept {
+        using SlotT = SlotFrom<SlotCtorT>;
+        return async_connection<value_type, error_type, SlotT>{
+            std::move(async_),
+            std::move(slot_ctor)(),
+            get_executor(),
         };
     }
 
-    executor& get_executor() { return exec::inline_executor(); }
+    static executor& get_executor() noexcept { return exec::inline_executor(); }
 
 private:
     async<T> async_;
@@ -72,8 +74,8 @@ private:
 
 template <typename T>
 struct as_signal<async<T>> {
-    constexpr static Signal<T, std::exception_ptr> auto call(async<T> async) {
-        return async_signal<T>{ /* .async = */ std::move(async) };
+    constexpr static Signal<T, std::exception_ptr> auto call(async<T> an_async) noexcept {
+        return async_signal<T>{ std::move(an_async) };
     }
 };
 
