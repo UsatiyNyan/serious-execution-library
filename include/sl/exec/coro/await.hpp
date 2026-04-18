c5023fec8afd9aeaa1b2e9c5f6280b3ea9f76880
UsatiyNyan <us4tiyny4n@gmail.com>
2026-04-18 20:12:36 +0200

    static and noexcept: coro

--- include/sl/exec/coro/await.hpp
+++ include/sl/exec/coro/await.hpp
@@ -6,7 +6,6 @@
 
 #pragma once
 
-#include "sl/exec/algo/emit/subscribe.hpp"
 #include "sl/exec/model/concept.hpp"
 
 #include <sl/meta/monad/maybe.hpp>
@@ -19,7 +18,7 @@ namespace sl::exec {
 namespace detail {
 
 template <typename ValueT, typename ErrorT>
-struct awaiter_slot final : slot<ValueT, ErrorT> {
+struct awaiter_slot final {
     struct awaiter_task final : task_node {
         explicit awaiter_task(std::coroutine_handle<> handle) : handle_{ std::move(handle) } {}
 
@@ -30,36 +29,42 @@ struct awaiter_slot final : slot<ValueT, ErrorT> {
         std::coroutine_handle<> handle_;
     };
 
-public:
-    awaiter_slot(
-        std::coroutine_handle<> handle,
-        meta::maybe<meta::result<ValueT, ErrorT>>& maybe_result,
-        executor& executor
-    )
-        : task_{ std::move(handle) }, maybe_result_{ maybe_result }, executor_{ executor } {}
-
-    void set_value(ValueT&& value) & override {
-        maybe_result_.emplace(tl::in_place, std::move(value));
-        executor_.schedule(task_);
-    }
-    void set_error(ErrorT&& error) & override {
-        maybe_result_.emplace(tl::unexpect, std::move(error));
-        executor_.schedule(task_);
-    }
-    void set_null() & override { task_.cancel(); }
-
-private:
     awaiter_task task_;
     meta::maybe<meta::result<ValueT, ErrorT>>& maybe_result_;
     executor& executor_;
+
+    void set_value(ValueT&& value) && noexcept {
+        maybe_result_.emplace(tl::in_place, std::move(value));
+        executor_.schedule(task_);
+    }
+    void set_error(ErrorT&& error) && noexcept {
+        maybe_result_.emplace(tl::unexpect, std::move(error));
+        executor_.schedule(task_);
+    }
+    void set_null() && noexcept { task_.cancel(); }
+};
+
+template <typename ValueT, typename ErrorT>
+struct awaiter_slot_ctor final {
+    std::coroutine_handle<> handle;
+    meta::maybe<meta::result<ValueT, ErrorT>>& maybe_result;
+    executor& ex;
+
+    constexpr awaiter_slot<ValueT, ErrorT> operator()() && noexcept {
+        return awaiter_slot<ValueT, ErrorT>{
+            .task_ = typename awaiter_slot<ValueT, ErrorT>::awaiter_task{ std::move(handle) },
+            .maybe_result_ = maybe_result,
+            .executor_ = ex,
+        };
+    }
 };
 
 template <SomeSignal SignalT>
 struct signal_awaiter final {
     using value_type = typename SignalT::value_type;
     using error_type = typename SignalT::error_type;
-    using slot_type = awaiter_slot<value_type, error_type>;
-    using connection_type = subscribe_connection<SignalT, slot_type>;
+    using slot_ctor_type = awaiter_slot_ctor<value_type, error_type>;
+    using connection_type = ConnectionFor<SignalT, slot_ctor_type>;
 
 public:
     explicit signal_awaiter(SignalT&& signal) : state_{ std::in_place_type<SignalT>, std::move(signal) } {}
@@ -69,9 +74,8 @@ public:
         DEBUG_ASSERT(std::holds_alternative<SignalT>(state_));
         auto signal = std::get<SignalT>(std::move(state_));
         auto& executor = signal.get_executor();
-        auto& a_connection = state_.template emplace<connection_type>( //
-            std::move(signal),
-            [&] { return slot_type{ std::move(handle), maybe_result_, executor }; }
+        auto& a_connection = state_.template emplace<connection_type>(
+            std::move(signal).subscribe(slot_ctor_type{ std::move(handle), maybe_result_, executor })
         );
         std::ignore = std::move(a_connection).emit();
     }
